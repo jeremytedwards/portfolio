d92993582c189ab40ea03233641787a190eda42c
Jeremy Edwards <jeremytedwards@gmail.com>
2016-02-01 23:40:16 -0800

    Added in title, author, pubdate to display

--- js/buildProjData.js
+++ js/buildProjData.js
@@ -1,4 +1,4 @@
-var project = [];
+var projects = [];
 
 function Project(data) {
   // Sample node from 'proj/data.js'
@@ -20,31 +20,27 @@ function Project(data) {
 }
 
 Project.prototype.toHtml = function() {
-  var $newProject = $('article.template').clone();
+  var $newProject = $('project.initHide').clone();
 
-  $newProject.attr('proj-category', this.category);
+  console.log($('project.initHide'));
+  console.log(this);
 
   // Sets the project title
   $newProject.find('h1').text(this.title);
 
   // Sets the project author name and url
-  $newProject.find('.project a').text(this.author).attr('href', this.authorUrl);
+  $newProject.find('.author').text(this.author);
 
   // Sets the publication date as a 'title' attribute to show on hover:
-  $newProject.find('time[pubdate]').attr('title', this.publishedOn);
+  $newProject.find('pubDate time[pubdate]').attr('title', this.publishedOn);
 
   // Display the date as a relative number of "days ago":
   $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
 
-  // Updates the project description body with html
-  $newProject.find('.project-desc').html(this.body);
-
-  // Adds horizontal rule under each project item
-  $newProject.append('<hr>');
-
   // It's ok to show if we have if we have content in the project data
-  // todo: validate that we have content in proj/data.js
   $newProject.removeClass('initHide');
+
+  console.log($newProject);
 
   return $newProject;
 }
@@ -54,9 +50,9 @@ projData.sort(function(a,b) {
 });
 
 projData.forEach(function(ele) {
-  project.push(new Project(ele));
+  projects.push(new Project(ele));
 })
 
-project.forEach(function(a){
-  $('#project').append(a.toHtml())
+projects.forEach(function(a){
+  $('#projects').append(a.toHtml())
 });
