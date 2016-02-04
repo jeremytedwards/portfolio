fe9ff21cc5b9808fbae5a114fe2be2423bc5e6ba
Jeremy Edwards <jerermytedwards@gmail.com>
2016-02-03 16:58:50 -0800

    Added images to the project data; Added styles to make project images responsive; New files: proj/img-proj/<images>

--- proj/data.js
+++ proj/data.js
@@ -5,7 +5,7 @@ var projData = [
     author:      'Jeremy Edwards',
     authorUrl:   'http://www.sample.com/project-01',
     publishedOn: '2016-02-01',
-    body:        '<p>This will be the first project in the portfolio</p><p>This will be the first project in the portfolio</p><p>This will be the first project in the portfolio</p><p>This will be the first project in the portfolio</p><p>This will be the first project in the portfolio</p>'
+    body:        '<p>This will be the first project in the portfolio</p><p>This will be the first project in the portfolio</p><p>This will be the first project in the portfolio</p><p>This will be the first project in the portfolio</p><img src="proj/img-proj/lion-svg.svg"></img><p>This will be the first project in the portfolio</p>'
   },
   {
     title:       'Project 03',
@@ -13,7 +13,7 @@ var projData = [
     author:      'Jeremy Edwards',
     authorUrl:   'http://www.sample.com/project-03',
     publishedOn: '2016-02-03',
-    body:        '<p>This will be the thrid project in the portfolio</p><p>This will be the thrid project in the portfolio</p><p>This will be the thrid project in the portfolio</p><p>This will be the thrid project in the portfolio</p><p>This will be the thrid project in the portfolio</p>'
+    body:        '<p>This will be the thrid project in the portfolio</p><p>This will be the thrid project in the portfolio</p><img src="proj/img-proj/che_guevara_black_white_line_art-555px.png"><p>This will be the thrid project in the portfolio</p><p>This will be the thrid project in the portfolio</p><p>This will be the thrid project in the portfolio</p>'
   },
   {
     title:       'Project 02',
@@ -21,6 +21,6 @@ var projData = [
     author:      'Jeremy Edwards',
     authorUrl:   'http://www.sample.com/project-02',
     publishedOn: '2016-02-02',
-    body:        '<p>This will be the second project in the portfolio</p><p>This will be the second project in the portfolio</p><p>This will be the second project in the portfolio</p><p>This will be the second project in the portfolio</p><p>This will be the second project in the portfolio</p>'
+    body:        '<p>This will be the second project in the portfolio</p><p>This will be the second project in the portfolio</p><p>This will be the second project in the portfolio</p><p>This will be the second project in the portfolio</p><img src="proj/img-proj/hand_with_ten_of_diamonds_black_white_line_art-555px.png"><p>This will be the second project in the portfolio</p>'
   },
 ];
